1fabf73ccfd809f362aea5dec912b8ea7dc7eecb
Piotr Gorecki <Piotr.Gorecki@partner.preh.de>
2018-11-27 18:52:33 +0100

    Chapter 7 2/3

--- 7/7_4/main.cpp
+++ 7/7_4/main.cpp
@@ -18,83 +18,67 @@ class lock_free_stack
 private:
     struct node;
 
-    struct counted_node_ptr
-    {
-        int external_count;
+    struct counted_node_ptr {
+        int8_t external_count;
         node * ptr;
     };
 
-    struct node
-    {
+    struct node {
         shared_ptr<T> data;
-        atomic<int> internal_count;
+        atomic<int8_t> internal_count;
         counted_node_ptr next;
 
-        node(T const & data_)
-            :
-            data(make_shared<T>(data_)),
-            internal_count(0)
-        {
-        }
+        node(T const & data_) : data(make_shared<T>(data_)), internal_count(0){}
     };
 
     atomic<counted_node_ptr> head;
 
-    void increase_head_count(counted_node_ptr & old_counter)
-    {
+    void increase_head_count(counted_node_ptr & old_counter) {
         counted_node_ptr new_counter;
-
-        do
-        {
+        do {
             new_counter = old_counter;
             ++new_counter.external_count;
-        }
-        while(!head.compare_exchange_strong(old_counter, new_counter, memory_order_acquire, memory_order_relaxed));
-
+        } while(!head.compare_exchange_strong(old_counter, new_counter, memory_order_acquire, memory_order_relaxed));
         old_counter.external_count = new_counter.external_count;
     }
 
 public:
-    ~lock_free_stack()
-    {
+    lock_free_stack() {
+        cout << "this lock free stack is really lock free: " << boolalpha << atomic_is_lock_free(&head) << endl;
+    }
+
+    ~lock_free_stack() {
         while(pop());
     }
 
-    void push(T const & data)
-    {
-        counted_node_ptr new_node;
-        new_node.ptr = new node(data);
+    void push(T const & data) {
+        counted_node_ptr new_node;     //Create new node
+        new_node.ptr = new node(data); //with data.
         new_node.external_count = 1;
-        new_node.ptr->next = head.load(memory_order_relaxed);
+        new_node.ptr->next = head.load(memory_order_relaxed); //Asign current head as a new_node->ptr.
+        //Make new node as a new head.
         while(!head.compare_exchange_weak(new_node.ptr->next, new_node, memory_order_release, memory_order_relaxed));
     }
 
-    shared_ptr<T> pop()
-    {
+    shared_ptr<T> pop() {
         counted_node_ptr old_head = head.load(memory_order_relaxed);
-        while(1)
-        {
+        while(1) {
             increase_head_count(old_head);
             node * const ptr = old_head.ptr;
-            if(!ptr)
-            {
+            if(!ptr) {
                 return shared_ptr<T>();
             }
-            if(head.compare_exchange_strong(old_head, ptr->next, memory_order_relaxed))
-            {
+            if(head.compare_exchange_strong(old_head, ptr->next, memory_order_relaxed)) {
                 shared_ptr<T> res;
                 res.swap(ptr->data);
 
                 int const count_increase = old_head.external_count-2;
 
-                if(ptr->internal_count.fetch_add(count_increase, memory_order_release) == -count_increase)
-                {
+                if(ptr->internal_count.fetch_add(count_increase, memory_order_release) == -count_increase) {
                     delete ptr;
                 }
                 return res;
-            }
-            else if(ptr->internal_count.fetch_add(-1, memory_order_relaxed)==1)
-            {
+            } else if(ptr->internal_count.fetch_add(-1, memory_order_relaxed)==1) {
                 ptr->internal_count.load(memory_order_acquire);
                 delete ptr;
             }
@@ -103,35 +87,31 @@ public:
     }
 };
 
-void f1(lock_free_stack<int> & st)
-{
-    for(auto & i : {1, 2, 3, 4, 5})
-    {
+void f1(lock_free_stack<int> & st) {
+    for(auto & i : {1, 2, 3, 4, 5}) {
         st.push(i);
     }
 }
 
-void f2(lock_free_stack<int> & st)
-{
-    for(auto & i : {6, 7, 8, 9, 10})
-    {
+void f2(lock_free_stack<int> & st) {
+    for(auto & i : {6, 7, 8, 9, 10}) {
         st.push(i);
     }
 }
 
-void f3(lock_free_stack<int> & st)
-{
-    for(int i = 0; i < 5; i++)
-    {
-        cout << *st.pop() <<  " ";
+void f3(lock_free_stack<int> & st) {
+    for(int i = 0; i < 5; i++) {
+        if(auto a = st.pop()) {
+            cout << *a <<  " ";
+        }
     }
 }
 
-void f4(lock_free_stack<int> & st)
-{
-    for(int i = 0; i < 5; i++)
-    {
-        cout << *st.pop() <<  " ";
+void f4(lock_free_stack<int> & st) {
+    for(int i = 0; i < 5; i++) {
+        if(auto a = st.pop()) {
+            cout << *a <<  " ";
+        }
     }
 }
 
@@ -139,18 +119,14 @@ int main()
 {
     lock_free_stack<int> st;
 
-    thread t1(f1, ref(st));
-    thread t2(f2, ref(st));
-    t1.join();
-    t2.join();
+    thread t1(f1, ref(st)); thread t2(f2, ref(st));
 
-    thread t3(f3, ref(st));
-    thread t4(f4, ref(st));
-    t3.join();
-    t4.join();
+    thread t3(f3, ref(st)); thread t4(f4, ref(st));
+
+    t1.join(); t2.join();
+
+    t3.join(); t4.join();
     cout << endl;
-
-    return 0;
 }
 
 
