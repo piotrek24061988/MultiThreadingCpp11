eb52a49dab7035b5086e9780e5de92eee8c120ef
Piotr Gorecki <Piotr.Gorecki@partner.preh.de>
2018-12-18 22:58:33 +0100

    chapter 9 almost done

--- 9/9_7/main.cpp
+++ 9/9_7/main.cpp
@@ -26,121 +26,94 @@ class interrupt_flag
     mutex set_clear_mutex;
 
 public:
-    interrupt_flag()
-        : thread_cond(nullptr), thread_cond_any(nullptr)
-    {}
+    interrupt_flag() : thread_cond(nullptr), thread_cond_any(nullptr) {}
 
-    void set()
-    {
+    void set() {
         flag.store(true, memory_order_relaxed);
         lock_guard<mutex> lk(set_clear_mutex);
-        if(thread_cond)
-        {
+        if(thread_cond) {
             thread_cond->notify_all();
-        }
-        else if(thread_cond_any)
-        {
+        } else if(thread_cond_any) {
             thread_cond_any->notify_all();
         }
     }
 
     template<typename Lockable>
-    void wait(condition_variable_any & cv, Lockable & lk)
-    {
+    void wait(condition_variable_any & cv, Lockable & lk) {
         struct custom_lock
         {
             interrupt_flag * self;
             Lockable & lk;
 
-            custom_lock(interrupt_flag * self_, condition_variable_any & cond, Lockable & lk_) : self(self_), lk(lk_)
-            {
+            custom_lock(interrupt_flag * self_, condition_variable_any & cond, Lockable & lk_) : self(self_), lk(lk_) {
                 self->set_clear_mutex.lock();
                 self->thread_cond_any=&cond;
             }
 
-            void unlock()
-            {
+            void unlock() {
                 lk.unlock();
                 self->set_clear_mutex.unlock();
             }
 
-            void lock()
-            {
-                lock(self->set_clear_mutex, lk);
+            void lock() {
+                std::lock(self->set_clear_mutex, lk);
             }
 
-            ~custom_lock()
-            {
+            ~custom_lock() {
                 self->thread_cond_any = 0;
                 self->set_clear_mutex.unlock();
             }
         };
+
         custom_lock cl(this, cv, lk);
         interruption_point();
         cv.wait(cl);
         interruption_point();
     }
 
-    bool is_set() const
-    {
+    bool is_set() const {
         return flag.load(memory_order_relaxed);
     }
 
-    void set_condition_variable(condition_variable & cv)
-    {
+    void set_condition_variable(condition_variable & cv) {
         lock_guard<mutex> lk(set_clear_mutex);
         thread_cond = &cv;
     }
 
-    void clear_condition_variable()
-    {
+    void clear_condition_variable() {
         lock_guard<mutex> lk(set_clear_mutex);
         thread_cond = 0;
     }
 };
-
 thread_local interrupt_flag this_thread_interrupt_flag;
 
-class thread_interrupted
-{
-};
+class thread_interrupted {};
 
-
-void interruption_point()
-{
-    if(this_thread_interrupt_flag.is_set())
-    {
+//To be colled in function done in separated thread.
+//Mark a point when this function execution can be innterupted.
+void interruption_point() {
+    if(this_thread_interrupt_flag.is_set()) {
         throw thread_interrupted();
     }
 }
 
-struct clear_cv_on_destruct
-{
-    ~clear_cv_on_destruct()
-    {
-        this_thread_interrupt_flag.clear_condition_variable();
-    }
-};
-
 template<typename Lockable>
-void interruptible_wait(condition_variable & cv, Lockable &lk)
-{
+void interruptible_wait(condition_variable_any & cv, Lockable &lk) {
     this_thread_interrupt_flag.wait(cv, lk);
 }
 
-template<typename T, typename Lockable>
-void interruptible_wait(future<T>& uf, Lockable &lk)
+template<typename T>
+void interruptible_wait(future<T>& uf)
 {
-    while(!this_thread_interrupt_flag.is_set())
-    {
-        if(uf.wait_for(lk, chrono::microseconds(1)) == future_status::ready)
-        {
+    while(!this_thread_interrupt_flag.is_set()) {
+        if(uf.wait_for(chrono::microseconds(1)) == future_status::ready) {
             break;
         }
         interruption_point();
     }
 }
 
+//Thread which work can be interrupted.
 struct interruptible_thread
 {
     thread internal_thread;
@@ -148,68 +121,112 @@ struct interruptible_thread
 
 public:
     template <typename FunctionType>
-    interruptible_thread(FunctionType f)
-    {
-        promise<interrupt_flag*>p;
-        internal_thread=thread([f, &p](){p.set_value(&this_thread_interrupt_flag);try{f();}catch(...){}});
+    interruptible_thread(FunctionType f) {
+        promise<interrupt_flag*>p;       //Initialize this_thread_interupt flag and run task.
+        internal_thread=thread([f, &p](){
+            p.set_value(&this_thread_interrupt_flag);
+            try{
+                f();
+            }catch(thread_interrupted const &){
+                cout << "thread_id: " << this_thread::get_id() << " interrupted" <<endl;
+            }
+        });
         flag = p.get_future().get();
     }
 
-    void join()
-    {
+    void join() {
         internal_thread.join();
     }
 
-    void detach();
-    bool joinable() const;
-    void interrupt()
-    {
-        if(flag)
-        {
+    void detach() {
+        internal_thread.detach();
+    }
+
+    bool joinable() const {
+        return internal_thread.joinable();
+    }
+
+    void interrupt() {//interrupt thread by calling srt function pn this_thread_interrupt_flag.
+        if(flag) {
             flag->set();
         }
     }
 };
 
-void f1()
-{
-    while(true)
-    {
-        interruption_point();
-        cout << "f1()" << endl;
+void foo1() {
+    for(int i = 0; i < 20; i++) {
+        if(i >= 10) interruption_point();
+        cout << "thread_id: " << this_thread::get_id() << ", i = " << i << endl;
         this_thread::sleep_for(1s);
     }
 }
 
-void f2()
-{
-    while(true)
-    {
-        interruption_point();
-        cout << "f2()" << endl;
-        this_thread::sleep_for(1s);
-    }
+void foo2() {
+    cout << "thread_id: " << this_thread::get_id() << " interruptible_wait1" << endl;
+    mutex lk;
+    condition_variable_any cv;
+    interruptible_wait(cv, lk);
+}
+
+void foo3() {
+    cout << "thread_id: " << this_thread::get_id() << " interruptible_wait2" << endl;
+    promise<int> pr;
+    future<int> fut = pr.get_future();
+    interruptible_wait(fut);
 }
 
 int main()
 {
-    vector<interruptible_thread> threads;
-    threads.push_back(interruptible_thread(f1));
-    threads.push_back(interruptible_thread(f2));
+    //Thread finalized because of work done.
+    vector<interruptible_thread> threads1;
+    threads1.push_back(interruptible_thread(foo1));
+    threads1.push_back(interruptible_thread(foo1));
 
-    this_thread::sleep_for(5s);
-
-    for(auto & t: threads)
-    {
-        t.interrupt();
-    }
-    for(auto & t : threads)
-    {
+    for(auto & t : threads1) {
         t.join();
     }
 
-    return 0;
+    //Threads finalized because of interruption.
+    vector<interruptible_thread> threads2;
+    threads2.push_back(interruptible_thread(foo1));
+    threads2.push_back(interruptible_thread(foo1));
+
+    this_thread::sleep_for(5s);
+
+    for(auto & t: threads2) {
+        t.interrupt();
+    }
+    for(auto & t : threads2) {
+        t.join();
+    }
+
+    //Threads finalized because of interruption
+    //during waiting.
+    vector<interruptible_thread> threads3;
+    threads3.push_back(interruptible_thread(foo2));
+    threads3.push_back(interruptible_thread(foo2));
+
+    this_thread::sleep_for(5s);
+
+    for(auto & t: threads3) {
+        t.interrupt();
+    }
+    for(auto & t : threads3) {
+        t.join();
+    }
+
+    //Threads finalized because of interruption
+    //during waiting2.
+    vector<interruptible_thread> threads4;
+    threads4.push_back(interruptible_thread(foo3));
+    threads4.push_back(interruptible_thread(foo3));
+
+    this_thread::sleep_for(5s);
+
+    for(auto & t: threads4) {
+        t.interrupt();
+    }
+    for(auto & t : threads4) {
+        t.join();
+    }
 }
-
-
-
